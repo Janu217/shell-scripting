e4c50b5ad52e60ae68ad87a207c2a8ec1180011f
Janaki D <Janu217@gmail.com>
2023-12-31 07:40:37 +0530

    auto push

---
+++ 01-shebang.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+# Start of the line in any bash script should always be a SHEBANG NOTATION.
+
+echo welcome to Bash scripting learning
+
+echo Bash is Shell scripting Born again Shell Scripting 
+
+# Pringing Multiple Lines 
+
+echo Line1
+echo Line2
+echo Line3
