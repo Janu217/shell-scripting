160d8811d877c3c8d2a7e69c722bb4ba1857450d
Janaki D <Janu217@gmail.com>
2023-12-31 07:41:36 +0530

    auto push

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -11,3 +11,10 @@ echo Bash is Shell scripting Born again Shell Scripting
 echo Line1
 echo Line2
 echo Line3
+
+#  \n  : new line
+# \t   : tab space                      
+
+echo -e "Line1\nLine2"
+
+echo -e "Line1\t\tLine2"
