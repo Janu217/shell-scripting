7dc55ea92d9b9802a4741967c73ef8cfc45cd428
Janaki D <Janu217@gmail.com>
2023-12-31 08:13:09 +0530

    auto push

---
+++ 05-specialvariables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+# Special variables gives special properties to your variables 
+
+# Here are few of the special variabes :  $0 to $9 , $? ,  $#,  $* , $@ 
+
+echo $0     # $0 prints the script name yoi're executing 
+echo "Name of the script executed is $0"
+echo "Name of the Trainer is $1"
+echo "Training Batch Number is $2"
+echo "Current Topics is $3" 
